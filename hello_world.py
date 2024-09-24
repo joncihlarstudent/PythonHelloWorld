f64ad319c63829071cd0f67f5c8fca2484522a0a
joncihlarstudent <36865633+joncihlarstudent@users.noreply.github.com>
2024-09-24 21:48:47 +0000

    Update hello_world.py by asking user for name.

--- hello_world.py
+++ hello_world.py
@@ -1,9 +1,12 @@
-
+"""
+This is my first program (or not!)
+J. Cihlar - September 2024
+"""
 
 
 def main():
-  pass
-
+  name: str = input("What is your name? ")
+  print("Hello", name, "!")
 
 
 if __name__ == "__main__":
